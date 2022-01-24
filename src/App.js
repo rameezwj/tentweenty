3aed49e3e39ee14038ea8349fd700b9a2622b797
rameez jawaid <rameez.jawaid@yayvo.com>
2022-01-24 12:07:26 +0500

    added locoscroll for smooth scrollung

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import './assets/styles.scss';
 import "./assets/helper.css";
 import Header from './components/header/Header.js';
@@ -6,6 +6,7 @@ import News from './components/news/News.js';
 import Featured from './components/featured/Featured.js';
 import Testimonial from './components/testimonial/Testimonial.js';
 import {hoverAnimation} from './helper.js';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 function App() {
   const [theme, setTheme] = useState("theme-light");
@@ -14,20 +15,31 @@ function App() {
     hoverAnimation();
   });
 
-
   const changeTheme = ()=>{
     theme === 'theme-dark' ? setTheme('theme-light') : setTheme('theme-dark');
   }
 
-  return (
-    <div className={`app_wrapper widthb100 ${theme}`}>
-      <Header changeTheme={changeTheme} />
-      <News />
-      <Testimonial />
-      <Featured />
+  const containerRef = useRef(null);
 
-      <div className="cursor"></div>
-    </div>
+  return (
+    <LocomotiveScrollProvider
+      options={
+        {
+          smooth: true,
+        }
+      }
+
+      containerRef={containerRef}
+    >
+      <div className={`app_wrapper widthb100 ${theme}`} data-scroll-container ref={containerRef}>
+        <Header changeTheme={changeTheme} />
+        <News />
+        <Testimonial />
+        <Featured />
+
+        <div className="cursor"></div>
+      </div>
+    </LocomotiveScrollProvider>
   );
 }
 
